3792d3ee1f43981b926b0da417b202f305ae365b
ajansoma <khadijahaabubakar@gmail.com>
2023-05-30 17:05:54 +0000

    backend

--- src/app.js
+++ src/app.js
@@ -28,7 +28,7 @@ app.use(morgan('combined'));
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(express.static(path.join('../client/dist')));
+// app.use(express.static(path.join('../client/dist')));
 
 app.use('/auth', authRouter);
 // app.use(verifyToken);
@@ -47,7 +47,7 @@ app.use((err, req, res, next) => {
 });
 
 app.get('/*', (req, res) => {
-  const indexPath = path.resolve('../client/dist/index.html');
+  // const indexPath = path.resolve('../client/dist/index.html');
   res.sendFile(indexPath);
 });
 
